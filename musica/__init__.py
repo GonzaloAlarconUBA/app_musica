84469a0ac729b4a9d4f24ba8ca43ed0d4823ac43
GonzaloAlarconUBA <espukidmt@gmail.com>
2024-05-13 11:52:31 -0300

    paginas de Artistas y Canciones creadas

--- musica/__init__.py
+++ musica/__init__.py
@@ -1,8 +1,35 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+with app.app_context():
+    from . import db
+    db.init_app(app)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/artistas')
+def artistas():
+    consulta = """
+        SELECT Name FROM artists
+    """
+
+    con = db.get_db()
+    res = con.execute(consulta)
+    lista_artistas = res.fetchall()
+    pagina = render_template('Artistas.html',artistas=lista_artistas)
+    return pagina
+
+@app.route('/canciones')
+def canciones():
+    consulta = """
+        SELECT Name FROM tracks
+    """
+
+    con = db.get_db()
+    res = con.execute(consulta)
+    lista_canciones = res.fetchall()
+    pagina = render_template('Canciones.html',canciones=lista_canciones)
+    return pagina
